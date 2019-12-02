758a74439463a0f20ff74ba391597f3b9a709205
Casey Martell <drmartell@gmail.com>
2019-12-02 14:54:08 -0800

    testing for getCaster includes all valid cases

--- __tests__/types.test.js
+++ __tests__/types.test.js
@@ -14,7 +14,7 @@ const {
 
 describe('validator module', () => {
   describe('basic validation', () => {
-    it('properly tells if a value is a number', () => {
+    it('properly determines if a value is a number', () => {
       expect(isNumber(3)).toBeTruthy();
       expect(isNumber('hi')).toBeFalsy();
       expect(isNumber([])).toBeFalsy();
@@ -22,15 +22,19 @@ describe('validator module', () => {
       expect(isNumber(() => {})).toBeFalsy();
       expect(isNumber(true)).toBeFalsy();
     });
-    it('properly tells if a value is a string', () => {
+
+    it('properly determines if a value is a string', () => {
       expect(isString('')).toBeTruthy();
     });
+
     it('propertly determines if a value is a boolean', () => {
       expect(isBoolean(true)).toBeTruthy();
     });
+
     it('properly determines if a value is an array', () => {
       expect(isArray([])).toBeTruthy();
     });
+
     it('propertly determines if a value is an object', () => {
       expect(isObject({})).toBeTruthy();
     }); 
@@ -43,15 +47,31 @@ describe('validator module', () => {
       expect(castToNumber(true)).toEqual(1);
       expect(castToNumber(false)).toEqual(0);
     });
-
     it('throws if value is not castable to number', () => {
       expect(() => castToNumber('hi')).toThrowErrorMatchingSnapshot();
       expect(() => castToNumber({})).toThrowErrorMatchingSnapshot();
     });
+
+    it('can cast values to a string', () => {
+      expect(castToString(3)).toEqual('3');
+      expect(castToString('3')).toEqual('3');
+      expect(castToString(true)).toEqual('true');
+      expect(castToString(false)).toEqual('false');
+    });
+    it('throws if value is not castable to number', () => {
+      expect(() => castToNumber('hi')).toThrowErrorMatchingSnapshot();
+      expect(() => castToNumber({})).toThrowErrorMatchingSnapshot();
+    });
+
+
   });
 
   it('can get the right caster', () => {
     expect(getCaster(Number)).toEqual(castToNumber);
+    expect(getCaster(String)).toEqual(castToString);
+    expect(getCaster(Boolean)).toEqual(castToBoolean);
+    expect(getCaster(Array)).toEqual(castToArray);
+    expect(getCaster(Object)).toBeNull();
     expect(getCaster(Promise)).toBeNull();
   });
 });
