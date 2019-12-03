9d3ec070032150def2affb6cc835ca99b681fb85
Casey Martell <drmartell@gmail.com>
2019-12-02 16:28:37 -0800

    add additional console.log statements

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const {
   isBoolean,
   isArray,
   isObject,
+  getCaster,
+  castToNumber,
+  castToString,
+  castToBoolean,
+  castToArray,
 } = require('./lib/types.js');
 
 console.log(isNumber('3'));
@@ -11,3 +16,11 @@ console.log(isString('3'));
 console.log(isBoolean('3'));
 console.log(isArray('3'));
 console.log(isObject('3'));
+console.log(getCaster(Number));
+console.log(getCaster(String));
+console.log(getCaster(Boolean));
+console.log(getCaster(Array));
+console.log(castToNumber('3'));
+console.log(castToString('3'));
+console.log(castToBoolean('3'));
+console.log(castToArray('3'));
