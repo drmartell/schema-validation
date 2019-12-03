6d824a0accf1f8d4702f2eb67048701717f51f98
Casey Martell <drmartell@gmail.com>
2019-12-02 16:06:19 -0800

    add console.log statements

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const { isNumber } = require('./lib/types.js');
+const { 
+  isNumber,
+  isString,
+  isBoolean,
+  isArray,
+  isObject,
+} = require('./lib/types.js');
 
 console.log(isNumber('3'));
+console.log(isString('3'));
+console.log(isBoolean('3'));
+console.log(isArray('3'));
+console.log(isObject('3'));
